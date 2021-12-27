3c2b5891a92b6ab06205fcad28e8255cb3742705
darkgenius <darkgenius@nextmail.ru>
2021-12-27 19:23:28 +0300

    Fix

--- components/GameField/GameField.tsx
+++ components/GameField/GameField.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet, Dimensions, View, Button } from "react-native";
+import { StyleSheet, Dimensions, View, Button, Text } from "react-native";
 import GestureRecognizer from "react-native-swipe-gestures";
 import { FieldModel, ROW_SIZE } from "../../models";
 import { getCellByCoords } from "../../utils";
@@ -60,28 +60,42 @@ const GameField: React.FC<GameFieldProps> = ({
   };
 
   return (
-    <View style={{ flexDirection: "column" }}>
-      {/* <GestureRecognizer
+    <View
+      style={{
+        flexDirection: "column",
+        flex: 1,
+        height: "100%",
+        backgroundColor: "yellow",
+        justifyContent: "center",
+        alignItems: "center",
+        //aspectRatio: 1,
+      }}
+    >
+      <GestureRecognizer
         config={gestureRecognizerConfig}
         onSwipeRight={handleSwipeRight}
         onSwipeLeft={handleSwipeLeft}
         onSwipeUp={handleSwipeUp}
         onSwipeDown={handleSwipeDown}
         style={{
-          //flexDirection: "row",
-          backgroundColor: "red",
-          flex: 1,
-          width: window.width - 20,
-          height: window.width - 20,
+          flexDirection: "row",
+          //backgroundColor: "red",
+          //flex: 1,
+          //width: window.width - 20,
+          //height: window.width - 20,
+          //alignSelf: "center",
+          //aspectRatio: 1,
         }}
       >
         <View style={styles.field}>{rows}</View>
-      </GestureRecognizer> */}
-      <View style={styles.field}>{rows}</View>
-      <Button title="swipe right" onPress={handleSwipeRight} />
-      <Button title="swipe left" onPress={handleSwipeLeft} />
-      <Button title="swipe up" onPress={handleSwipeUp} />
-      <Button title="swipe down" onPress={handleSwipeDown} />
+      </GestureRecognizer>
+      {/* <View style={styles.field}>{rows}</View> */}
+      <View style={styles.debugControls}>
+        <Button title="swipe right" onPress={handleSwipeRight} />
+        <Button title="swipe left" onPress={handleSwipeLeft} />
+        <Button title="swipe up" onPress={handleSwipeUp} />
+        <Button title="swipe down" onPress={handleSwipeDown} />
+      </View>
     </View>
   );
 };
@@ -92,18 +106,25 @@ const styles = StyleSheet.create({
     width: window.width - 20,
     height: window.width - 20,
     backgroundColor: "#837a70",
+    //aspectRatio: 1,
+    //height: "auto",
     margin: 10,
-    //borderWidth: 1,
+    borderWidth: 1,
     borderRadius: 10,
     padding: 10,
-    // paddingBottom: 5,
-    // paddingTop: 5,
-    justifyContent: "space-between",
+    paddingBottom: 5,
+    paddingTop: 5,
+    //justifyContent: "space-between",
+    //alignSelf: "center",
   },
   row: {
-    //flex: 1,
+    flex: 1,
     flexDirection: "row",
     justifyContent: "space-between",
+    alignItems: "center",
+  },
+  debugControls: {
+    flex: 1,
   },
 });
 
